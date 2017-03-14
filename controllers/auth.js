91013aebcfb13126155bb3931f0c842d12edea90
GLassiter <gregory.lassiter@routematch.com>
2017-03-14 13:30:58 -0400

    pre-publsh to npm

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,7 +11,7 @@ exports.auth = function(req, res) {
    
 
 exports.login = function(req, res) {
-  var dataaccess = require('../data_access/data-access.js');  
+  var dataaccess = require('../data-access.js');  
   dataaccess.rmSql('PA_DuFast','spFRGetHeadwayInformation','Date!dt!Tue Feb 28 2017 07:31:03 GMT-0500 (Eastern Standard Time)|SubrouteIDs!t!')
   .then((results) => {
     res.send(results);    
